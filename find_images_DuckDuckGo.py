918a5294abb21f7603e01ade6e7f9eb2adba195b
Ali <alisonm712@gmail.com>
2022-09-01 09:12:39 +0100

    Gets images from DDG and exports learner

---
+++ find_images_DuckDuckGo.py
@@ -0,0 +1,105 @@
+from fastai.vision.all import *
+import gradio as gr
+import fastbook
+fastbook.setup_book()
+from fastbook import *
+from ipywidgets import *
+import streamlit as st
+
+# Download images of different bear categories
+def download_images_ddg(img_category, img_types, num_images):
+    if not path.exists():
+        path.mkdir()
+        for o in img_types:
+            dest = (path/o)
+            dest.mkdir(exist_ok=True)
+            results = search_images_ddg(f'{str(o)} {img_category}', max_images=num_images)
+            for u in range(len(results)):
+                try:
+                    download_url(url=results[u], 
+                                 dest=f'{dest}/{str(o)}-{str(u+1)}.jpg', 
+                                 timeout=400, 
+                                 show_progress=False)
+                except:
+                    print(f'not found {results[u]}')
+                    continue
+
+# Define image category and image types
+img_category = 'cat'
+img_types = ['bengal','siamese','ragdoll', 'maine coon', 'persian', 'british shorthair', 'sphynx', 'abyssinian', 'scottish fold',
+'birman', 'russian blue', 'calico', 'norwegian forest cat', 'manx', 'havana brown', 'highlander', 'cornish rex', 'egyptian mau', 
+'ragamuffin', 'oriental shorthair', 'american shorthair', 'moggie']
+path = Path(img_category)
+
+# Call function to download images
+download_images_ddg(img_category, img_types, num_images=100)
+
+# Check for failed images
+fns = get_image_files(path)
+failed = verify_images(fns)
+failed.map(Path.unlink);
+
+class DataLoaders(GetAttr):
+    def __init__(self, *loaders): self.loaders = loaders
+    def __getitem__(self, i): return self.loaders[i]
+    train,valid = add_props(lambda i,self: self[i])
+
+cats = DataBlock(
+    blocks=(ImageBlock, CategoryBlock), 
+    get_items=get_image_files, 
+    splitter=RandomSplitter(valid_pct=0.2, seed=42),
+    get_y=parent_label,
+    item_tfms=Resize(128))
+
+dls = cats.dataloaders(path)
+
+learn = vision_learner(dls, resnet18, metrics=error_rate)
+learn.fine_tune(4)
+
+learn_inf = load_learner(path/'export.pkl')
+
+
+learn.export('catbreeds.pkl')
+#learn_inf = load_learner(path/'export.pkl')
+#learn_inf.predict('images/bengal.jpg')
+btn_upload = widgets.FileUpload()
+btn_upload
+
+
+img = PILImage.create(btn_upload.data[-1])
+
+out_pl = widgets.Output()
+out_pl.clear_output()
+with out_pl: display(img.to_thumb(128,128))
+out_pl
+
+pred,pred_idx,probs = learn_inf.predict(img)
+
+lbl_pred = widgets.Label()
+lbl_pred.value = f'Prediction: {pred}; Probability: {probs[pred_idx]:.04f}'
+lbl_pred
+
+btn_run = widgets.Button(description='Classify')
+btn_run
+
+def on_click_classify(change):
+    img = PILImage.create(btn_upload.data[-1])
+    out_pl.clear_output()
+    with out_pl: display(img.to_thumb(128,128))
+    pred,pred_idx,probs = learn_inf.predict(img)
+    lbl_pred.value = f'Prediction: {pred}; Probability: {probs[pred_idx]:.04f}'
+
+btn_run.on_click(on_click_classify)
+
+tn_upload = widgets.FileUpload()
+
+VBox([widgets.Label('Select your cat!'), 
+      btn_upload, btn_run, out_pl, lbl_pred])
+
+
+
+
+
+
+
+
